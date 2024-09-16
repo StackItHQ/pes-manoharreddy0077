9a3399a346a2b2a7ca5d66783c0ffed98c87ff39
manoharreddy0077 <118151632+manoharreddy0077@users.noreply.github.com>
2024-09-16 13:06:37 +0530

    updated db schema

--- db.py
+++ db.py
@@ -9,26 +9,63 @@ cursor.execute('''
 CREATE TABLE IF NOT EXISTS employees (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    position TEXT NOT NULL
+    position TEXT NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments (id)
 )
 ''')
 
+
 cursor.execute('''
-INSERT INTO employees (name, position)
-VALUES
-    ('John Doe', 'Software Engineer'),
-    ('Jane Smith', 'Data Scientist'),
-    ('Emily Johnson', 'Product Manager')
+CREATE TABLE IF NOT EXISTS departments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+)
 ''')
+
+
 cursor.execute('''
-INSERT INTO employees (name, position)
-VALUES
-    ('John Doe1', 'Software Engineer'),
-    ('Jane Smith2', 'Data Scientist'),
-    ('Emily Johnson3', 'Product Manager')
+CREATE TABLE IF NOT EXISTS projects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments (id)
+)
 ''')
+
+
+cursor.execute('''
+INSERT INTO departments (name)
+VALUES
+    ('Engineering'),
+    ('Marketing'),
+    ('Product Development')
+''')
+
+
+cursor.execute('''
+INSERT INTO employees (name, position, department_id)
+VALUES
+    ('John Doe', 'Software Engineer', 1),
+    ('Jane Smith', 'Data Scientist', 1),
+    ('Emily Johnson', 'Product Manager', 3),
+    ('Michael Brown', 'Marketing Specialist', 2),
+    ('Linda Davis', 'Product Designer', 3)
+''')
+
+
+cursor.execute('''
+INSERT INTO projects (name, department_id)
+VALUES
+    ('Project Alpha', 1),
+    ('Project Beta', 2),
+    ('Project Gamma', 3),
+    ('Project Delta', 1),
+    ('Project Epsilon', 3)
+''')
+
 
 conn.commit()
 conn.close()
 
-print("Table 'employees' created and data inserted.")
+print("Tables 'employees', 'departments', and 'projects' created and data inserted.")
