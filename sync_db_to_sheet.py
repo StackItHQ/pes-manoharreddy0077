988081865ae4c62769f1af767e79c918f6dfab9f
manoharreddy0077 <118151632+manoharreddy0077@users.noreply.github.com>
2024-09-16 13:04:49 +0530

    removed

--- sync_db_to_sheet.py
+++ sync_db_to_sheet.py
@@ -1,86 +1,86 @@
-import sqlite3
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import schedule
-import time
+# import sqlite3
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
+# import schedule
+# import time
 
 
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('credential.json', scope)
-client = gspread.authorize(creds)
-spreadsheet_id = '19EW46Fb2En1AaYA5gXiBYzfWc5dBt2BBmvJrqyuxk1o'
-sheet = client.open_by_key(spreadsheet_id).sheet1
+# scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+# creds = ServiceAccountCredentials.from_json_keyfile_name('credential.json', scope)
+# client = gspread.authorize(creds)
+# spreadsheet_id = '19EW46Fb2En1AaYA5gXiBYzfWc5dBt2BBmvJrqyuxk1o'
+# sheet = client.open_by_key(spreadsheet_id).sheet1
 
 
-DATABASE = 'mydatabase.db'
+# DATABASE = 'mydatabase.db'
 
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect(DATABASE)
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
-def update_database_from_sheet():
-    try:
-        data = sheet.get_all_values()
+# def update_database_from_sheet():
+#     try:
+#         data = sheet.get_all_values()
 
-        conn = get_db_connection()
-        cursor = conn.cursor()
+#         conn = get_db_connection()
+#         cursor = conn.cursor()
 
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS employees (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            position TEXT NOT NULL
-        )
-        ''')
-
-
-        cursor.execute('DELETE FROM employees')
+#         cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS employees (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             name TEXT NOT NULL,
+#             position TEXT NOT NULL
+#         )
+#         ''')
 
 
-        for row in data[1:]:  
-            cursor.execute('INSERT INTO employees (id, name, position) VALUES (?, ?, ?)', (row[0], row[1], row[2]))
+#         cursor.execute('DELETE FROM employees')
 
 
-        conn.commit()
-        conn.close()
-
-        print('Database updated from Google Sheet successfully.')
-    except Exception as e:
-        print(f'Error updating database from Google Sheet: {e}')
-
-def update_sheet_from_database():
-    try:
-
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        cursor.execute('SELECT * FROM employees')
-        rows = cursor.fetchall()
+#         for row in data[1:]:  
+#             cursor.execute('INSERT INTO employees (id, name, position) VALUES (?, ?, ?)', (row[0], row[1], row[2]))
 
 
-        data = [['ID', 'Name', 'Position']]  
-        for row in rows:
-            data.append(list(row))
+#         conn.commit()
+#         conn.close()
+
+#         print('Database updated from Google Sheet successfully.')
+#     except Exception as e:
+#         print(f'Error updating database from Google Sheet: {e}')
+
+# def update_sheet_from_database():
+#     try:
+
+#         conn = get_db_connection()
+#         cursor = conn.cursor()
+
+#         cursor.execute('SELECT * FROM employees')
+#         rows = cursor.fetchall()
 
 
-        sheet.update('A1', data)
-
-        conn.close()
-
-        print('Google Sheet updated from database successfully.')
-    except Exception as e:
-        print(f'Error updating Google Sheet from database: {e}')
-
-def sync():
-    update_sheet_from_database()
-    update_database_from_sheet()
+#         data = [['ID', 'Name', 'Position']]  
+#         for row in rows:
+#             data.append(list(row))
 
 
-schedule.every(10).minutes.do(sync) 
+#         sheet.update('A1', data)
 
-if __name__ == "__main__":
-    print("Starting synchronization...")
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+#         conn.close()
+
+#         print('Google Sheet updated from database successfully.')
+#     except Exception as e:
+#         print(f'Error updating Google Sheet from database: {e}')
+
+# def sync():
+#     update_sheet_from_database()
+#     update_database_from_sheet()
+
+
+# schedule.every(10).minutes.do(sync) 
+
+# if __name__ == "__main__":
+#     print("Starting synchronization...")
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
